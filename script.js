b5d6913fb4c6c754c9e1a2ff2e5916d81216b418
Perbi Onasis Obuobi-Dei <65021040+perbi-onasis@users.noreply.github.com>
2024-05-07 11:31:36 +0000

    commented the script to troubleshoot the login switch error

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+/***$('.form').find('input, textarea').on('keyup blur focus', function (e) {
   
     var $this = $(this),
         label = $this.prev('label');
@@ -41,3 +41,5 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
     $(target).fadeIn(600);
     
   });
+
+  ****/
